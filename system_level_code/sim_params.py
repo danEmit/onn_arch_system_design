f53289580b044720c5feddf4ffd8ae9cbfb51ff0
D <dansturm@uw.edu>
2022-04-25 22:57:46 -0700

    minor changes

--- system_level_code/sim_params.py
+++ system_level_code/sim_params.py
@@ -3,6 +3,14 @@ SS_inOut_file_path = base_directory + "results/" + "untracked" + "/"
 config_file_path  = base_directory + "configs/scale.cfg"
 
 
+NN_file_name = "test"
+NN_file_path_local = "topologies/ONN/"
+NN_file_path_local = NN_file_path_local.replace("/", "_") 
+NN_file_path_local_name = NN_file_path_local + NN_file_name
+
+SS_results_file_path_name = SS_inOut_file_path + NN_file_path_local_name + "/" + NN_file_path_local_name + "__SS_results.csv"
+chip_specs_file_path_name = SS_inOut_file_path + NN_file_path_local_name + "/" + NN_file_path_local_name + "__SS_results_chip_specs.csv"
+
 
 make_plots = 0
 run_system_specs = 1

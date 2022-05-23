9452eac17a6837572aec84e48aa8f4ec67082328
D <dansturm@uw.edu>
2022-05-23 17:46:38 -0400

    minor update

--- system_level_code/analytical_modeling.py
+++ system_level_code/analytical_modeling.py
@@ -8,12 +8,7 @@ import pandas as pd
 import sim_params
 
 
-extra_specs_basic = ["Row Fold", "Col Fold", "Num Programs", "Num Conv in Input (for batch 1)", "Num Conv in Input (Batch)"]
-extra_specs_adv = ["Necessary Accumulator Depth for Batch 1, \nno Col ReProgram", "Necessary Accumulator Depth for Batch 1, \nFull Col ReProgram", \
-     "Col Fold * Batch Product, \nGiven Accumulator Size", "Needed SRAM Input Size \nGiven the Accumulator Size \n(Function of Allowed Batch Size with No Col ReProgram)", \
-          "Needed SRAM Input Size \nGiven the Accumulator Size \n(Function of Allowed Batch Size with Full Col ReProgram)"]
-
-dp = 2
+dec_points = 2
 
 
 # note using 
@@ -55,10 +50,8 @@ def acquire_NN_SS_info(SS_inputs_dict, NN_file_path_name):
 
 
 
-def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standalone = 0, accumulator_elements_col = 0):
+def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name):
      outputs_names = specs_info.SS_outputs_names.copy()
-     if standalone:
-          outputs_names.extend(extra_specs_basic + extra_specs_adv)
      outputs_data = [0] * len(outputs_names)
      model_outputs = pd.DataFrame(index = outputs_names)
      
@@ -81,6 +74,68 @@ def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standal
           num_conv_in_input_batch = num_conv_in_input * batch_size
           compute_cycles = num_programs * num_conv_in_input_batch
 
+          SRAM_input_reads = num_conv_in_input_batch * filter_size * col_fold
+          SRAM_output_writes = num_conv_in_input_batch * row_fold * num_filter[layer]
+          SRAM_filter_reads = filter_size * num_filter[layer]
+
+          DRAM_filter_reads = -1
+          DRAM_input_reads = -1
+          DRAM_output_writes = SRAM_output_writes
+          
+          col_name = "AM " + str(index)
+          model_outputs.at[specs_info.SS_outputs_names, col_name] = [SRAM_input_reads, SRAM_filter_reads, \
+               SRAM_output_writes, DRAM_input_reads, DRAM_filter_reads, DRAM_output_writes,\
+                     num_programs, compute_cycles]
+     
+     totals = model_outputs.sum(axis = 1)
+     model_outputs.at[:, "AM total"] = totals
+     return(model_outputs)
+
+     x = 1
+
+
+def make_analytical_model_acc(SS_inputs_dict, NN_file_path_name, accumulator_elements_col, batch_size):
+     outputs_names = specs_info.acc_related_specs.copy()
+     outputs_data = [0] * len(outputs_names)
+     model_outputs = pd.DataFrame(index = outputs_names)
+     
+     (input_rows, input_cols, filter_rows, filter_cols, channels, num_filter, strides, SA_rows, SA_cols, SRAM_input, SRAM_filter, SRAM_output) = acquire_NN_SS_info(SS_inputs_dict, NN_file_path_name)
+     num_layers = len(input_rows)
+
+     for index, layer in enumerate(range(num_layers)):
+          filter_size = filter_rows[layer] * filter_cols[layer] * channels[layer]
+          row_fold_theor = math.ceil(filter_size / SA_rows)
+          col_fold_theor = math.ceil(num_filter[layer] / SA_cols)
+
+          conv_rows = math.ceil((input_rows[layer] - filter_rows[layer]) / strides[layer]) + 1
+          conv_cols = math.ceil((input_cols[layer] - filter_cols[layer]) / strides[layer]) + 1
+          #convRows = math.ceil(input_rows[layer] / strides[layer])
+          #convCols = math.ceil(input_cols[layer] / strides[layer])
+          num_conv_in_input = conv_rows * conv_cols 
+
+          ## need to compute specs for full col fold and col fold of one 
+          ## note that it's now more about what batch size is allowed than what batch size we pick, right? 
+          
+          batch_col_fold_product = accumulator_elements_col / num_conv_in_input
+          if (batch_col_fold_product < 1):
+               print("accumulator not big enough, try other value")
+               # then write -1 to all fields
+               # return
+
+          # if col fold = 1
+          col_fold_mini = 1
+          batch_size_mini = batch_col_fold_product
+
+          
+
+
+          
+
+          num_programs = row_fold * col_fold
+
+          num_conv_in_input_batch = num_conv_in_input * batch_size
+          compute_cycles = num_programs * num_conv_in_input_batch
+
           #avg_rows_used = filter_size / (SA_rows * row_fold)
           SRAM_input_reads = num_conv_in_input_batch * filter_size * col_fold
           SRAM_output_writes = num_conv_in_input_batch * row_fold * num_filter[layer]
@@ -92,14 +147,7 @@ def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standal
           DRAM_input_reads = -1
           DRAM_output_writes = SRAM_output_writes
           
-          col_name = "AM " + str(index)
-          model_outputs.at[specs_info.SS_outputs_names, col_name] = [SRAM_input_reads, SRAM_filter_reads, \
-               SRAM_output_writes, DRAM_input_reads, DRAM_filter_reads, DRAM_output_writes,\
-                     num_programs, compute_cycles]
-          if not standalone:
-               continue
 
-          model_outputs.at[extra_specs_basic, col_name] = [row_fold, col_fold, num_programs, num_conv_in_input, num_conv_in_input_batch]
 
           needed_acc_depth_batch_1_no_col_fold = num_conv_in_input
           needed_acc_depth_batch_1_max_col_fold = needed_acc_depth_batch_1_no_col_fold * col_fold
@@ -108,10 +156,10 @@ def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standal
           needed_input_SRAM_size_given_acc_size_full_cold_fold = needed_input_SRAM_size_given_acc_size_no_cold_fold / col_fold
 
 
-          model_outputs.at[extra_specs_adv, col_name] = [round(needed_acc_depth_batch_1_no_col_fold, 2), \
-               round(needed_acc_depth_batch_1_max_col_fold, 2), round(batch_col_fold_product_given_acc, 2), \
-                    round(needed_input_SRAM_size_given_acc_size_no_cold_fold, 2), \
-                         round(needed_input_SRAM_size_given_acc_size_full_cold_fold, 2)]
+          model_outputs.at[extra_specs_adv, col_name] = [round(needed_acc_depth_batch_1_no_col_fold, dec_points), \
+               round(needed_acc_depth_batch_1_max_col_fold, dec_points), round(batch_col_fold_product_given_acc, dec_points), \
+                    round(needed_input_SRAM_size_given_acc_size_no_cold_fold, dec_points), \
+                         round(needed_input_SRAM_size_given_acc_size_full_cold_fold, dec_points)]
                
      
           
@@ -122,6 +170,7 @@ def make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standal
      return(model_outputs)
 
      x = 1
+
 
 
 
@@ -149,7 +198,7 @@ def main():
                          "DRAM Bandwidth Mode": sim_params.DRAM_mode}) 
 
 
-     analytical_model = make_analytical_model(SS_inputs_dict, batch_size, NN_file_path_name, standalone, accumulator_elements_col)
+     analytical_model = make_analytical_model(SS_inputs_dict, NN_file_path_name, accumulator_elements_col)
      analytical_model.to_csv(analytical_model_file_path_name)
 
 

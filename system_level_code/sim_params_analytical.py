5e831087bedde22a1c13d6ef654b7df479970f83
dan <dansturm@uw.edu>
2022-06-21 19:30:06 -0700

    clean up sim params file

--- system_level_code/sim_params_analytical.py
+++ system_level_code/sim_params_analytical.py
@@ -4,7 +4,7 @@ import numpy as np
 mega = 10 ** 6
 
 make_plots = 0
-run_system_specs = 0
+run_system_specs = 1
 make_plots = run_system_specs and make_plots
 
 base_directory = "/Users/d/Desktop/onn_arch_system_design/"
@@ -23,26 +23,7 @@ plots_folder_complete = results_file_base_folder + plots_folder
 
 all_layers = []
 if (0):
-     if (1):
-          input_height  = [10, 5]
-          input_width   = [10, 5] 
-          filter_height = [3, 4]
-          filter_width  = [3, 4]
-          channels = [2, 1]
-          num_filter = [10, 3]
-          strides = [1, 1]
-
-     num_layers = len(input_height) 
-     all_layers = []
-     for layer in range(num_layers):
-          layer_dict = {"Input Height" : input_height[layer], "Input Width" : input_width[layer], "Filter Height" : filter_height[layer], "Filter Width": filter_width[layer], \
-               "Channels" : channels[layer], "Num Filter" : num_filter[layer], "Strides" : strides[layer]}
-          all_layers.append(layer_dict)
-          
-     for ind in range(len(channels) - 1):
-          if (num_filter[ind] != channels[ind + 1]):
-               print("\nNote that this is not a valid NN, the number of filters in layer ", str(ind), " is different from the number of channels in layer ", str(ind + 1), sep = "")
-
+     x = 1 
 else:
      input_rows = [] 
      input_cols  = []
@@ -77,47 +58,10 @@ else:
                     if all_filters_size > max_all_filters_size:
                          max_all_filters_size = all_filters_size
 
-
-
                except:
                     print("can't convert all the data in NN file to ints")
 
-
-
-SRAM_input_size_options  = [1000000]#, 2000000]#, 5000000, 10000000]
-SRAM_filter_size_options = [ 500000]#, 1000000]#, 2000000,  5000000]
-SRAM_output_size_options = [ 300000]#, 1000000]#, 2000000,  5000000]
-
-array_rows = 16
-array_cols = 16
-array_rows_options = [8, 16, 32, 64, 128, 256]
-array_cols_options = [8, 16, 32, 64, 128, 256]
-array_size_options = []
-for array_rows_slider in array_rows_options:
-     for array_rows_slider in array_cols_options:
-          array_size_options.append([array_rows_slider, array_rows_slider])
-
-min_SRAM_filter_size = max(array_rows_options) * max(array_cols_options)
-SRAM_filter_size = int(max_all_filters_size)
-SRAM_filter_size_options = np.linspace(min_SRAM_filter_size, max_all_filters_size, 4).astype(int)
-
-
-SRAM_input_size_options = [int(x * max_single_input_size) for x in [1.1, 3.1, 5.1, 10.1]]
-SRAM_input_size = int(max_single_input_size * 1.1)
-
-SRAM_output_size_options = SRAM_input_size_options
-SRAM_output_size = SRAM_input_size
-
-batch_size_options = [1, 8, 16, 32, 64, 128]
-batch_size = 1
-
-accumulator_elements_options = [20000, 100000, 1000000]
-accumulator_elements = 20000
-
-SRAM_input_size_options  = [1000000]
-SRAM_filter_size_options = [1000000]
-SRAM_output_size_options = [1000000]
-
+dual_core = 1
 symbol_rate_options = [10]
 
 ## array rows
@@ -193,9 +137,68 @@ accumulator_elements = 20000
 single_setting = [array_rows, array_cols, SRAM_input_size, SRAM_filter_size, SRAM_output_size, accumulator_elements, batch_size]
 
 
+'''
+
+SRAM_input_size_options  = [1000000]#, 2000000]#, 5000000, 10000000]
+SRAM_filter_size_options = [ 500000]#, 1000000]#, 2000000,  5000000]
+SRAM_output_size_options = [ 300000]#, 1000000]#, 2000000,  5000000]
+
+array_rows = 16
+array_cols = 16
+array_rows_options = [8, 16, 32, 64, 128, 256]
+array_cols_options = [8, 16, 32, 64, 128, 256]
+array_size_options = []
+for array_rows_slider in array_rows_options:
+     for array_rows_slider in array_cols_options:
+          array_size_options.append([array_rows_slider, array_rows_slider])
+
+min_SRAM_filter_size = max(array_rows_options) * max(array_cols_options)
+SRAM_filter_size = int(max_all_filters_size)
+SRAM_filter_size_options = np.linspace(min_SRAM_filter_size, max_all_filters_size, 4).astype(int)
+
+
+SRAM_input_size_options = [int(x * max_single_input_size) for x in [1.1, 3.1, 5.1, 10.1]]
+SRAM_input_size = int(max_single_input_size * 1.1)
+
+SRAM_output_size_options = SRAM_input_size_options
+SRAM_output_size = SRAM_input_size
+
+batch_size_options = [1, 8, 16, 32, 64, 128]
+batch_size = 1
+
+accumulator_elements_options = [20000, 100000, 1000000]
+accumulator_elements = 20000
+
+SRAM_input_size_options  = [1000000]
+SRAM_filter_size_options = [1000000]
+SRAM_output_size_options = [1000000]
 
 
 
+'''
+
+'''
+if (1):
+          input_height  = [10, 5]
+          input_width   = [10, 5] 
+          filter_height = [3, 4]
+          filter_width  = [3, 4]
+          channels = [2, 1]
+          num_filter = [10, 3]
+          strides = [1, 1]
+
+     num_layers = len(input_height) 
+     all_layers = []
+     for layer in range(num_layers):
+          layer_dict = {"Input Height" : input_height[layer], "Input Width" : input_width[layer], "Filter Height" : filter_height[layer], "Filter Width": filter_width[layer], \
+               "Channels" : channels[layer], "Num Filter" : num_filter[layer], "Strides" : strides[layer]}
+          all_layers.append(layer_dict)
+          
+     for ind in range(len(channels) - 1):
+          if (num_filter[ind] != channels[ind + 1]):
+               print("\nNote that this is not a valid NN, the number of filters in layer ", str(ind), " is different from the number of channels in layer ", str(ind + 1), sep = "")
+
+'''
 
 
 

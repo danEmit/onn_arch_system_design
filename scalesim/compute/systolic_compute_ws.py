69abb5e59acd3c03dbcc44a622163bbb3838625c
dan <dansturm@uw.edu>
2023-02-13 17:57:37 -0800

    some change i made a while back b/c the original ss code causes errors... i can't remember the exact logic for what's going on here but i'm pretty sure my changes are correct

--- scalesim/compute/systolic_compute_ws.py
+++ scalesim/compute/systolic_compute_ws.py
@@ -184,7 +184,10 @@ class systolic_compute_ws:
         inter_fold_gap_prefix = self.arr_row
         inter_fold_gap_prefix_mat = np.ones((inter_fold_gap_prefix, self.arr_row)) * -1
 
-        inter_fold_gap_suffix = self.arr_row + self.arr_col - 2
+        # my change is necesssary to counteract their update, their's causes issues
+        #inter_fold_gap_suffix = self.arr_row + self.arr_col - 2
+        inter_fold_gap_suffix = self.arr_col - 1
+
         #The last input needs self.arr_row - 1 cycles to reach the last column of PE array and then self.arr_col - 1 cycles to reduce along the last column.
 
         inter_fold_gap_suffix_mat = np.ones((inter_fold_gap_suffix, self.arr_row)) * -1

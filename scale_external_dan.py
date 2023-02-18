d7bd4a504a12d68d91f283b9edfd4661dc5ee07a
dan <dansturm@uw.edu>
2023-02-17 17:05:47 -0800

    random space added for visual aesthetic

--- scale_external_dan.py
+++ scale_external_dan.py
@@ -104,6 +104,7 @@ def run_scale_sim(hardware_arch, NN_layers):
 	endPostProcessTime = time.time()
 	SS_execution_time = round((endExecutionTime - startExecutionTime) / 60, 3)
 	SS_post_process_time = round((endPostProcessTime - endExecutionTime) / 60, 3)
+	SS_results.loc[" "] = " "
 	SS_results.loc["Simulation Run Time [min]"] = SS_execution_time
 	SS_results.loc["Simulation Post Process Time [min]"] = SS_post_process_time
 	return(SS_results)
